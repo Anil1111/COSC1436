f2e77e3af31895112a97840de53bb34439cc6c46
michaeltccd <30909640+michaeltccd@users.noreply.github.com>
2017-08-10 14:39:52 -0500

    Add files via upload

---
+++ Chapter 2/PartsOfCPlusPlus/Main.h
@@ -0,0 +1,12 @@
+/*
+ * Provides demonstration of IO routines.
+ */
+
+//PREPROCESSOR DIRECTIVE - Standard header file directive
+#if !defined(PARTSOFCPLUSPLUS_H_INCLUDED)
+#define PARTSOFCPLUSPLUS_H_INCLUDED
+
+//PREPROCESSOR DIRECTIVE - Compiler specific dirctive
+#pragma once
+
+#endif
